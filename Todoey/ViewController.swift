8987442744418fd9ab4b096adf42a8527a289a57
allwak <oksamytny@phystech.edu>
2019-10-04 19:05:25 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Yuriy Oksamytny on 04/10/2019.
+//  Copyright © 2019 Yuriy Oksamytny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
